9e919cbeb50d8bf7ccefd98a96e0611b3595a673
zyy”git config --global user.name Yod <zhangyuan9980211@gmail.com>
2021-05-19 16:19:20 +0100

    plot

---
+++ plot_test.jl
@@ -0,0 +1,30 @@
+# using Revise
+# using Plots,SymPy,LaTeXStrings
+
+
+
+function s(x::Float64)
+    return abs(floor(x + 1 / 2) - x)
+end
+
+function test(x::Float64)
+    aa = 2 + s(x);
+    return aa
+end
+
+function f(w::Float64, x::Float64)
+    Taka = 0;
+    for n in 1:100
+        Taka += w^n * s(2^n * x)
+    end
+    return Taka
+end
+
+fig = plot(); # produces an empty plot
+for w = [0.1, 0.25,0.5,0.75,0.9] # the for loop
+    plot!(fig, [-f(w, x) for x ∈ 0:0.0001:0.25]) # the loop fills in the plot with this
+end
+plot(fig, label=[L"w=0.1" L"w=0.25" L"w=0.5" L"w=0.75" L"w=0.9"],
+    fg_legend=:transparent, legend=:topleft) # just produces the plot
+display(fig)
+
