5e1c2888937c218a13fda609323d44e3987bb195
zyy”git config --global user.name Yod <zhangyuan9980211@gmail.com>
2021-05-23 22:55:35 +0100

    add minimization2

--- mini.jl
+++ mini.jl
@@ -1,5 +1,5 @@
 
-include("plot_test.jl")
+# include("plot_test.jl")
 using NOMAD
 
 function s(x)
@@ -9,44 +9,27 @@ end
 function f(w, x)
     Taka = 0;
     for n in 1:100
-        Taka += w^n * s(2^n * x)
+        Taka += w^n * s(2^n * x[1]) 
     end
     return Taka
 end
 
-function c(x)
-    return -1
-end
-
-function eval_fct(x)
-    bb_outputs = [-f(0.9, x), c(x)]
+function eval_fun(x)
+    w = 0.9
+    bb_outputs = [-f(w, x)]
     success = true
     count_eval = true
     return (success, count_eval, bb_outputs)
 end
+
 pb = NomadProblem(1, # number of inputs of the blackbox
-  2, # number of outputs of the blackbox
-  ["OBJ", "EB"], # type of outputs of the blackbox
-  eval_fct,
-  lower_bound=[-0.],
-  upper_bound=[0.25])
-result = NOMAD.solve(pb, [0.1])
+                  1, # number of outputs of the blackbox
+                  ["OBJ"], # type of outputs of the blackbox
+                  eval_fun;
+                #   granularity=0.01 * ones(Float64, 1),
+                  lower_bound=[0.],
+                  upper_bound=[0.25],
+                #   min_mesh_size=[1e-9]
+                  )
 
-
-
-# function eval_fun(x::Float64)
-#     w = 0.9
-#     bb_outputs = [-f(w, x)]
-#     success = true
-#     count_eval = true
-#     return (success, count_eval, bb_outputs)
-# end
-
-# pb = NomadProblem(1, # number of inputs of the blackbox
-#                   1, # number of outputs of the blackbox
-#                   ["OBJ"], # type of outputs of the blackbox
-#                   eval_fun;
-#                   lower_bound=[0.],
-#                   upper_bound=[0.25])
-
-# result = NOMAD.solve(pb, [0.1])
+result = NOMAD.solve(pb, [0.14])
